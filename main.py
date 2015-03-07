4a972181e9b82a3bf20f44fedc61319d77cb6ac3
= <=>
2015-03-06 22:23:22 -0800

    Script will now sort through the files, send resluts to sqlite3  database, and print out how many attacks have come from each IP address...

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import re
+import sqlite3
 
+def periodsToUnderscores(ipaddress):
+   tableName='ip_' + ipaddress.replace('.','_')
+   return tableName
+   
+con = sqlite3.connect('intruders')
+cur = con.cursor()
 logfile = open('auth.txt', 'r')
 count = 0
 line = "start"
 lineOut = ''
+intruders = []
 while line:
    line=logfile.readline()
    logParts = line.split(']: ')
@@ -14,20 +22,25 @@ while line:
          hackDate += "-0" + rawDate[1] + "-" + rawDate[2]
       else:
          hackDate += "-" + rawDate[1] + "-" + rawDate[2]
-      lineOut = hackDate + ' '
-      print hackDate
-      hackDate = ''
       ipaddr = re.findall( r'[0-9]+(?:\.[0-9]+){3}', logParts[1])
       if len(ipaddr)>=1:
-         print ipaddr[0]
-	  
-   #  messageParts=logParts[1].split(' ')
-   #for i in range (0,len(messageParts)):
-   #   if(messageParts[i] == 'from'):
-   #      lineOut +=  messageParts[i + 1].replace(':','')
-   #     print lineOut
-   #   elif(messageParts[i].startswith("rhost=")):
-   #      addressParts=messageParts[1].split('=')
-   #      print addressParts
-   #      #lineOut += addressParts[1]
-			
+         tableName=periodsToUnderscores(ipaddr[0])
+         isThereQuery = 'SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'' + tableName + '\''
+         isThere=cur.execute(isThereQuery)
+         isTable=isThere.fetchone()
+         if(isTable == None):
+            isTable = isThere.fetchone()    
+            makeTableCommand='create table ' + tableName + '(hacker char(15), hackDate char(15))'
+            sqlCommand=sqlCommand='insert into ' + tableName + ' values(\'' + ipaddr[0] +  '\',\'' + hackDate + '\')'
+            cur.execute(makeTableCommand)
+            cur.execute(sqlCommand)
+            intruders.append(tableName)            
+         else:
+            sqlCommand='insert into ' + tableName + ' values(\'' + ipaddr[0] +  '\',\'' + hackDate + '\')'
+            cur.execute(sqlCommand)
+for g in range(len(intruders)):
+   countSqlCommand='select count(*) from ' + intruders[g]
+   numAttackQuery=cur.execute(countSqlCommand);
+   numAttacks=numAttackQuery.fetchone()
+   print intruders[g] + '  number of attacks = ' + str(numAttacks[0])
+            
