1acfe29d6be03a800a90e19142a3c3ba48d4199f
Mike Wyatt <uranusdemilo@gmail.com>
2015-05-06 08:43:15 -0700

    Added ip address output for copy-and-paste from ssh client.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import re
 import sqlite3
+import sys
 
 def periodsToUnderscores(ipaddress):
    tableName='ip_' + ipaddress.replace('.','_')
    return tableName
+
+def createAddress(tableName):
+   tableName=tableName.replace('ip_','')
+   tableName=tableName.replace('_','.')
+   return tableName
+   
+def makeSpaces(num,max):
+   sp = max - num
+   if sp != 0:
+      for i in range(0,sp):
+         sys.stdout.write(' ')
+   else:
+      sys.stdout.write('')
+   
    
 con = sqlite3.connect('intruders')
 cur = con.cursor()
-logfile = open('auth.txt', 'r')
+logfile = open('auth.log', 'r')
 count = 0
 line = "start"
 lineOut = ''
@@ -42,5 +57,11 @@ for g in range(len(intruders)):
    countSqlCommand='select count(*) from ' + intruders[g]
    numAttackQuery=cur.execute(countSqlCommand);
    numAttacks=numAttackQuery.fetchone()
-   print intruders[g] + '  number of attacks = ' + str(numAttacks[0])
-            
+   print intruders[g],
+   makeSpaces(len(intruders[g]),18)
+   print '  number of attacks = ',
+   makeSpaces(len(str(numAttacks[0])),5)
+   print numAttacks[0],
+   print ' ',
+   print createAddress(intruders[g])
+
--- test.py
+++
@@ -1,5 +0,0 @@
-import re
-str1='this is an long buch of text with address=192.168.1.223 embedded'
-ipaddr = re.findall( r'[0-9]+(?:\.[0-9]+){3}',str1 )
-print ipaddr
-
